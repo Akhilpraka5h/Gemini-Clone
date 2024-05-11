a726614f704ac0b153911e1a77bd1fdc2f8f1d4d
Akhilpraka5h <134758922+Akhilpraka5h@users.noreply.github.com>
2024-05-11 14:07:06 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./component/Sidebar/Sidebar";
+import Mian from "./component/Main/Mian";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Mian />
+    </>
+  );
+};
+
+export default App;
